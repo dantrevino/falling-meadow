37b26d706cad306567c25d5de6f78157ac0f0d4c
Dan Trevino <dantrevino@gmail.com>
2017-01-14 20:39:57 -0800

    update stateful & stateless widget examples

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData (
+        primarySwatch: Colors.blue
+      ),
+      home: new StatefulWidgetExample("Hello, World")
+    )
+  );
+}
+
+class StatelessWidgetExample extends StatelessWidget {
+  final String _appBarTitle;
+
+  StatelessWidgetExample(this._appBarTitle) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(_appBarTitle)
+      ),
+      body: new Center (
+        child: new Text('Demo')
+      )
+    );
+  }
+}
+
+class StatefulWidgetExample extends StatefulWidget {
+  final String _appBarTitle;
+
+  StatefulWidgetExample(this._appBarTitle, { Key key }) : super(key: key);
+
+  @override
+  _StatefulWidgetExampleState createState() => new _StatefulWidgetExampleState(_appBarTitle);
+}
+
+class _StatefulWidgetExampleState extends State<StatefulWidgetExample> {
+  final String _appBarTitle;
+
+  int _counter = 0;
+
+  _StatefulWidgetExampleState(this._appBarTitle);
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(this._appBarTitle)
+      ),
+      body: new Center(
+        child: new Text('Button tapped $_counter time${ _counter == 1 ? '' : 's' }.')
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add)
+      )
+    );
+  }
+}
